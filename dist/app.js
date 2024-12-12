fa0c8691ca844bc6abaf804bbc9c752214af5c2d
shinji530 <shinji53059@gmail.com>
2024-12-12 14:23:40 +0900

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// import express from 'express';
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send();
+// });
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
+console.log('Hello World');
