95085bb92ec091037ac1c6d968ae2c210997fe6b
shinji530 <shinji53059@gmail.com>
2024-12-13 09:43:04 +0900

    주석 추가

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,16 @@
 import { User } from "../types/user";
 import { IUserRepository } from "../repositories/user.repository.interface";
 
+// service layer: 실제 비즈니스 로직이 작성되는 곳곳
 export class UserService {
+  // IUserRepository를 구현하는 어떠한 repository가 와도 괜찮다.
+  // 비즈니스 로직의 영향이 없이 db가 바뀔 수 있는 구조조
   constructor(private userRepository: IUserRepository) {}
+  
   async createUser (data: Pick<User, 'email' | 'name'>): Promise<User> {
     return this.userRepository.create(data);
   }
+  
   async getUserById(id: string): Promise<User | null> {
     return this.userRepository.findById(id);
   }
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,5 @@
 export interface User {
   id: string;
   email: string;
-  name?: string;
+  name?: string | null;
 }
